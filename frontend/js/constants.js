8bb46aff1c17bbeaf39661a26264ac64c4acef6d
Ardit Bajraktari <ditzyxv69@gmail.com>
2022-05-21 17:32:18 +0200

    newupdate

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,10 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x8a3E11feC33C2344532586003AdacEA0AE6A7c44"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the AlienGang NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_p = 'The AlienGang NFT Project is a collection of 5000 unique Alien NFTS & unique digital collectibles, Purchase one NFT and Join The Alien Gang';
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
