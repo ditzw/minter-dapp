1a0924a6b4b3873fb6c1affea54ccfd92a8517a2
Ardit Bajraktari <ditzyxv69@gmail.com>
2022-05-23 01:12:44 +0200

    w

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,7 +60,7 @@ const TOKENS_PER_MINT = 100; // maximum number of NFTs a user can mint in a sing
 const PUBLIC_MINT_START_DATE = "2022-05-24T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-24T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = ""; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xAB4D8CD6092a6FA91e3B61289b6E7bF6586143ba"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
